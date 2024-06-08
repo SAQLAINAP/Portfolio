38aa76b7f36c8a6bec372586f1e4dc44064e20a3
Saqlain <saqlainahmed302@gmail.com>
2024-06-08 22:26:39 +0530

    made major updates

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontSize: {
+          '4.5xl': '2.625rem', // Custom size between 4xl and 5xl
+        },
+      },
+    },
+    plugins: [],
+  };
+  
